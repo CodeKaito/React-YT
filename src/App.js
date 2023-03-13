61e3096dbfdb5cd48c9d1d80847ddb3543d1a4ac
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-13 16:44:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Hello World! </h1>
-      </header>
-    </div>
+    <BrowserRouter>
+    </BrowserRouter>
   );
 }
 

a4e7dd4029d05b30cac79eb276d34b15472c5da2
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-14 11:13:39 +0100

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -12,10 +12,12 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: 'auto', height='90vh', flex: 2}}>
+      <Box p={2} sx={{ overflowY: 'auto', height:'90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: '#2E2D2D'}}> New
           <span style={{ color: 'F31503'}}> videos </span>
         </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
